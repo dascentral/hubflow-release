78b83184e5afc1f3b426ecbe1574faa979cb27f3
Douglas Smith <smithda@gmail.com>
2017-03-26 18:27:44 -0400

    Initial import of bumpver repo contents

---
+++ src/PackageJson.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Dascentral\Bump\Console;
+
+class PackageJson
+{
+    /**
+     * The file that contains the content.
+     *
+     * @var mixed
+     */
+    protected $file = './package.json';
+
+    /**
+     * The parsed contents of "package.json".
+     *
+     * @var mixed
+     */
+    protected $packageJson;
+
+    /**
+     * Create a new instance of this class.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->packageJson = (file_exists($this->file)) ? json_decode(file_get_contents($this->file)) : null;
+    }
+
+    /**
+     * Getter for the full package.json contents.
+     *
+     * @return mixed
+     */
+    public function contents()
+    {
+        return $this->packageJson;
+    }
+
+    /**
+     * Getter for the name within the package.json.
+     *
+     * @return string
+     */
+    public function name()
+    {
+        return isset($this->packageJson->name) ? $this->packageJson->name : null;
+    }
+
+    /**
+     * Getter for the version within the package.json.
+     *
+     * @return string
+     */
+    public function version()
+    {
+        return isset($this->packageJson->version) ? $this->packageJson->version : null;
+    }
+
+    /**
+     * Bump the version accordingly.
+     *
+     * @param  string $type
+     * @return void
+     */
+    public function bump($type)
+    {
+        $this->updateVersion($type);
+        $this->save();
+    }
+
+    /**
+     * Determine the new version based upon type.
+     *
+     * @param  string $type
+     * @return void
+     */
+    protected function updateVersion($type)
+    {
+        list($version_major, $version_minor, $version_patch) = explode('.', $this->packageJson->version);
+        switch ($type) {
+            case 'minor':
+                $version_minor++;
+                $version_patch = 0;
+                break;
+
+            case 'major':
+                $version_major++;
+                $version_minor = 0;
+                $version_patch = 0;
+                break;
+
+            default:
+                $version_patch++;
+                break;
+        }
+
+        $this->packageJson->version = $version_major . '.' . $version_minor . '.' . $version_patch;
+    }
+
+    /**
+     * Save the contents of the current object to disk.
+     *
+     * @return void
+     */
+    protected function save()
+    {
+        if ($fh = fopen($this->file, 'w')) {
+            $json = json_encode($this->packageJson, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+            fwrite($fh, $json);
+        }
+    }
+}
---
+++ src/VersionCommand.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Dascentral\Bump\Console;
+
+use Dascentral\Bump\Console\PackageJson;
+use RuntimeException;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Process;
+
+class VersionCommand extends Command
+{
+    /**
+     * The parsed contents of the "package.json" from the current directory.
+     *
+     * @var Dascentral\Bump\Console\PackageJson
+     */
+    protected $packageJson;
+
+    /**
+     * Create a new instance of this class.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->packageJson = new PackageJson();
+    }
+
+    /**
+     * Configure the command options.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this->setName('version')
+             ->setDescription('Bump the version of package.json.')
+             ->addArgument('type', InputArgument::OPTIONAL);
+    }
+
+    /**
+     * Execute the command.
+     *
+     * @param  \Symfony\Component\Console\Input\InputInterface  $input
+     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+     * @return void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $type = ($input->getArgument('type')) ? strtolower($input->getArgument('type')) : 'patch';
+
+        // Confirm that the "package.json" file exists
+        if (!$this->packageJson->contents()) {
+            $output->writeln("\n" . '<error>No package.json in current directory.</error>' . "\n");
+            exit(1);
+        }
+
+        // We track the current version for script output
+        if (!$initial_version = $this->packageJson->version()) {
+            $output->writeln("\n" . '<error>No version information found within the package.json.</error>' . "\n");
+            exit(1);
+        }
+
+        // Bump the application version
+        $this->packageJson->bump($type);
+
+        // Share output with the user
+        $this->outputResult($output, $initial_version);
+    }
+
+    /**
+     * Output result to the user.
+     *
+     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+     * @param  string $initial_version
+     * @return void
+     */
+    protected function outputResult($output, $initial_version)
+    {
+        $message = ($this->packageJson->name()) ? 'Updating <info>' . $this->packageJson->name() . '</info>.' : '<info>Updating application.</info>';
+        $message .= ' (<comment>v' . $initial_version . '</comment> => <comment>v' . $this->packageJson->version() . '</comment>)';
+        $output->writeln("\n" . $message . "\n");
+    }
+}
